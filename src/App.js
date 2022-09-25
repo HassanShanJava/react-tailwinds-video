26a998e0b55d6eab0a8a7cadd714509f6e787653
HassanShanJava <hassanshan675@gmail.com>
2022-09-25 21:36:16 +0500

    added destinations, nxt search

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Destinations from "./components/Destinations";
 import Hero from "./components/Hero";
 import NavBar from "./components/NavBar";
 
@@ -7,6 +8,7 @@ function App() {
     <div>
       <NavBar/>
       <Hero/>
+      <Destinations/>
     </div>
   );
 }
